686be8fd55eeff4e94ccd07f613b5a3db639ce1a
Kaze Wong <kazewong.physics@gmail.com>
2023-08-09 14:58:23 -0400

    zz

--- athena/read_file.py
+++ athena/read_file.py
@@ -5,7 +5,8 @@ sys.path.append('/mnt/sw/nix/store/6plcgbnkn9w8nf1y0dzmjs1jbjbzi4ma-openvdb-10.0
 import athena_read
 import pyopenvdb as vdb
 
-path = '/mnt/home/wwong/ceph/Simulations/Athena/collapse'
+path = '/mnt/home/wwong/ceph/Simulations/Athena/blast3d/Blast.out1.'
+#path = '/mnt/home/wwong/ceph/Simulations/Athena/collapse/Collapse.out2.'
 output_name = '/mnt/home/wwong/ceph/Visualization/Tutorial/Athena/collapse'
 N_res = 128
 
@@ -15,14 +16,7 @@ for name in field:
 	data[name] = []
 
 for index in range(51):
-	# data_prim = athena_read.athdf('/mnt/home/wwong/ceph/Simulations/Athena/blast3d/Blast.out1.'+str(index).zfill(5)+'.athdf')
-	data_prim = athena_read.athdf('/mnt/home/wwong/ceph/Simulations/Athena/collapse/Collapse.out2.'+str(index).zfill(5)+'.athdf')
-	x = data_prim['x1f'] # x pos of cell faces
-	y = data_prim['x2f'] # y pos of cell faces
-	z = data_prim['x3f'] # z pos of cell faces
-	xv = data_prim['x1v'] # x pos of cell centers
-	yv = data_prim['x2v'] # y pos of cell centers
-	zv = data_prim['x3v'] # z pos of cell centers
+	data_prim = athena_read.athdf(path+str(index).zfill(5)+'.athdf')
 	data['rho'].append(data_prim['rho']) # density at cell center
 	data['press'].append(data_prim['press']) # pressure at cell center
 	data['vel1'].append(data_prim['vel1']) # x velocity at cell center
