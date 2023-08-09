ed7ace2afe14fd36032c5bfcb7f72389a652f7e7
kazewong <kazewong.physics@gmail.com>
2023-08-09 12:44:31 -0400

    Add blast

--- athena/npz_to_vdb.py
+++ athena/npz_to_vdb.py
@@ -1,35 +1,20 @@
 import numpy as np
 import pyopenvdb as vdb
 
-def Scale_data(data):
-	return data/(data.max()-data.min())
-
-data = np.load('./IllustrisTNG_128.npz')
-output_name = 'CCA_tutorial_illustrisTNG.vdb'
-
-N_res = 256
-Mgas = Scale_data(data['Mgas'])
-T = Scale_data(data['T'])
-Z = Scale_data(data['Z'])
+data = np.load('blast3d.npz')
+N_res = 64
 
 
+output_name = 'blast3d'
 
-dataCube = []
+field_name = ['rho','press', 'vel1', 'vel2', 'vel3']
 
-dataCube.append(vdb.FloatGrid())
-dataCube[-1].copyFromArray(Mgas)
-dataCube[-1].name = 'Mgas'
-dataCube[-1].transform = vdb.createLinearTransform(voxelSize=1/(N_res))
+for i in range(len(data['rho'])):
+	dataCube = []
 
-dataCube.append(vdb.FloatGrid())
-dataCube[-1].copyFromArray(T)
-dataCube[-1].name = 'T'
-dataCube[-1].transform = vdb.createLinearTransform(voxelSize=1/(N_res))
-
-dataCube.append(vdb.FloatGrid())
-dataCube[-1].copyFromArray(Z)
-dataCube[-1].name = 'Z'
-dataCube[-1].transform = vdb.createLinearTransform(voxelSize=1/(N_res))
-
-
-vdb.write(output_name, grids=dataCube)
+	for name in field_name:
+		dataCube.append(vdb.FloatGrid())
+		dataCube[-1].copyFromArray(data[name][i])
+		dataCube[-1].name = name
+		dataCube[-1].transform = vdb.createLinearTransform(voxelSize=1/(N_res))
+		vdb.write(output_name+'_'+str(i)+'.vdb', grids=dataCube)
